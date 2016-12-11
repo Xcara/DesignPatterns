0da42976fc320b4af13f148efc6fe7d8fd9d6a2c
Xcara <1390796199@qq.com>
2016-12-11 21:07:28 +0800

    Update ICommand.h

--- Undo_Redo/Undo_Redo2/ICommand.h
+++ Undo_Redo/Undo_Redo2/ICommand.h
@@ -2,8 +2,8 @@
 //  ICommand.h
 //  History
 //
-//  Created by 徐艺然 on 16/11/29.
-//  Copyright © 2016年 徐艺然. All rights reserved.
+//  Created by xu on 16/11/29.
+//  Copyright © 2016 xu. All rights reserved.
 //
 
 #pragma once
